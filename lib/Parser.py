b8bca25b8f85237aa9e27e4330ec3f383520c1ba
youhana-hana <meyouhana@gmail.com>
2017-03-18 17:30:36 +0000

    parse matrix

--- lib/Parser.py
+++ lib/Parser.py
@@ -1,2 +1,13 @@
 class Parser:
-    pass
+    """ Parse string matrix and covert each entry to double value, skipping nan values"""
+    def parse(self,  matrix):
+        result = []
+        for row in range(0, len(matrix)):
+            result.append([])
+            rowLength = len(matrix[row])
+            for col in range(0, rowLength):
+                value = matrix[row][col];
+                if value !='nan':
+                    value = float(value)
+                result[row].append(value)
+        return result
