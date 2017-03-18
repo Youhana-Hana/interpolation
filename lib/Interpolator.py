4302c0cbbfd1220e69cb7c737d53fffccea47120
youhana-hana <meyouhana@gmail.com>
2017-03-18 18:20:29 +0000

    added Interpolator

---
+++ lib/Interpolator.py
@@ -0,0 +1,50 @@
+class Interpolator:
+    """ Interpolate matrix and calculate average for all entries with nan value"""
+    def interpolate(self,  matrix):
+        result = []
+        for row in range(0, len(matrix)):
+            result.append([])
+            rowLength = len(matrix[row])
+            for col in range(0, rowLength):
+                value = matrix[row][col];
+                if value =='nan':
+                    value = self.getAverage(matrix, row, col)
+                result[row].append(value)
+        return result
+
+    def getAverage(self, matrix, row, col):
+        neighbours = self.get_neighbours(matrix, row, col)
+        return self.calculate_average(neighbours)
+
+    def get_neighbours(self, matrix, row, col):
+        matrixLength = len(matrix);
+        rowLength = len(matrix[row])
+
+        neighbours = []
+        if(col > 0):
+            value  = matrix[row][col -1]
+            self.push_value(neighbours, value)
+
+        if(col < rowLength - 1):
+            value  = matrix[row][col + 1]
+            self.push_value(neighbours, value)
+
+        if(row > 0):
+            value  = matrix[row - 1][col]
+            self.push_value(neighbours, value)
+
+        if(row < matrixLength - 1):
+            value  = matrix[row + 1][col]
+            self.push_value(neighbours, value)
+
+        return neighbours
+
+    def calculate_average(self, neighbours):
+        print(neighbours)
+        total = sum(neighbours);
+        count = len(neighbours);
+        return round(float(total) / float (count), 6)
+
+    def push_value(self, values, value):
+        if(value != 'nan'):
+            values.append(value)
---
+++ test/test_Interpolator.py
@@ -0,0 +1,32 @@
+import sys
+sys.path.append('./lib')
+import unittest
+import mock
+import math
+from Interpolator import Interpolator
+
+class Test_Interpolator(unittest.TestCase):
+    def test_construct(self):
+        interpolator= Interpolator()
+        self.assertIsNotNone(interpolator)
+
+    def test_read_with_nan_values(self):
+            input = [[37.454012, 95.071431, 73.199394, 59.865848 , 'nan'], [15.599452, 5.808361, 86.617615, 60.111501, 70.807258], [2.058449, 96.990985, 'nan', 21.233911, 18.182497], ['nan', 30.424224, 52.475643, 43.194502, 29.122914], [61.185289, 13.949386, 29.214465, 'nan', 45.606998]]
+            expected = [[37.454012, 95.071431, 73.199394, 59.865848 , 65.336553], [15.599452, 5.808361, 86.617615, 60.111501, 70.807258], [2.058449, 96.990985, 64.329538, 21.233911, 18.182497], [31.222654, 30.424224, 52.475643, 43.194502, 29.122914], [61.185289, 13.949386, 29.214465, 39.338655, 45.606998]]
+            interpolator = Interpolator()
+            
+            actual = interpolator.interpolate(input)
+
+            self.assertEqual(expected, actual)
+
+    # def test_read_invalid_number_of_rows(self):
+    #     with mock.patch('__builtin__.input', side_effect = ['not valid']):
+    #         expected = None
+    #         transport = Transport()
+
+    #         actual = transport.read()
+
+    #         self.assertEqual(expected, actual)
+    
+if __name__ == '__main__':
+    unittest.main()
